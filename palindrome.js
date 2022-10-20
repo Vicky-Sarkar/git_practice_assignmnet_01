bfe9f80cb7a36f8f75b5cf0ec229b5203718f8da
vickysarkar7 <vickyysarkar@gmail.com>
2022-10-20 22:31:25 +0530

    prime file and palindrome files are created

---
+++ palindrome.js
@@ -0,0 +1,17 @@
+//Writing code for chicking a string palindrome or not 
+//<<-------Code writen by Vicky Sarkar------>>
+
+
+let str = "civic";
+let bag = "";
+for (i = str.length - 1; i >= 0; i++) {
+
+	bag += str1[j]
+
+}
+
+if (str1 == bag) {
+	consoile.logi(str, "Is a palidrome word")
+} else {
+	consoile.logi(str1, "Is not a palidrome Word")
+}
---
+++ prime.js
@@ -0,0 +1,16 @@
+//Writing code for chicking a number prime or not 
+//<<-------Code writen by Vicky Sarkar------>>
+
+let num =184
+let count=0
+for(let i=0;i<=num;i++){
+	if(num%i==0){
+		count++
+	}
+}
+
+if(count==count){
+	console.log(Num,"Number is prime number")
+}else{
+	console.log(num,"Number is not a prime number")
+}
