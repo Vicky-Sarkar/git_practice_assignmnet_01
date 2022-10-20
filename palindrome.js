0ed051c3a1f4552c4ffd5b6d9377bf3a72f4907b
Vicky Sarkar <96650163+Vicky-Sarkar@users.noreply.github.com>
2022-10-20 23:20:19 +0530

    Update palindrome.js

--- palindrome.js
+++ palindrome.js
@@ -5,11 +5,12 @@
 let str = "civic";
 let N=str.length-1
 for(let i =0;i<=N/2; i++) {
-     if(str[i]!=str[N-1-i]{
-	console.log("No")
+     if(str[i]!=str[N-1-i]){
+	 console.log("Yes")
         break;
      }else{
-        console.log("Yes")
+	     console.log("No")
+       
          break;
      }
 }
